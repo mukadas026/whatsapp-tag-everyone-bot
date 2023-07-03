0ee3bcf82ce46e1e93c8251af378f3f5eab138ba
mukadas026 <mukadasm026@gmail.com>
2023-07-03 23:42:57 +0000

    refine code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, LocalAuth} = require("whatsapp-web.js")
+const { Client, LocalAuth } = require("whatsapp-web.js")
 const qrcode = require("qrcode-terminal")
 console.log("initializing")
 const client = new Client({
@@ -27,38 +27,39 @@ client.on("message_create", async (message) => {
 	console.log("Group chat:", chat.name)
 
 	let result = ""
-	
+
 	// check if a participant of the group prompts the bot with "/everyone"
-	if (message.body === "/everyone") {
-		// Get a list of members in the group chat
-		const participants = await chat.participants
-		let partic = {}
+	if (!(message.body === "/everyone")) return
+	// Get a list of members in the group chat
+	const participants = await chat.participants
+	let partic = {}
 
-		// check if the sender is an Admin
-		for (const participant of participants){
-			if (participant.id._serialized === message._data.id.participant){
-				partic = participant
-				break
-			}
-		}
-		if(!partic.isAdmin) return
-		let contacts = []
-		// get the contact details of each participant 
-		for (const participant of participants) {
-			const contact = await client.getContactById(participant.id._serialized)
-			// excluding the bot itself
-			if (contact.number !== (await message.getContact()).number) {
-				contacts.push(contact)
-				result += `@${contact.name} `
-			}
-		}
-		// send message to group chat and mention everyone 
-		chat.sendStateTyping()
-		chat.sendMessage('', { mentions: contacts, sendSeen: false })
-		// chat.sendMessage(result)
-
-		chat.clearState()
+	// check if the sender is an Admin
+	for (const participant of participants) {
+		if (!(participant.id._serialized === message._data.id.participant)) continue
+		partic = participant
+		break
 	}
+	if (!partic.isAdmin) return
+	let contacts = []
+	// get the contact details of each participant
+	for (const participant of participants) {
+		const contact = await client.getContactById(participant.id._serialized)
+		// excluding the bot itself
+		const botContact = await message.getContact()
+		// const botNumber = botContact.number
+		// console.log(botContact)
+		// if (contact.id.user === botContact.id.user) continue
+		console.log("started everyone", contacts)
+		contacts.push(contact)
+		result += `@${contact.name} `
+	}
+	// send message to group chat and mention everyone
+	chat.sendStateTyping()
+	chat.sendMessage("", { mentions: contacts, sendSeen: false })
+	// chat.sendMessage(result)
+
+	chat.clearState()
 })
 
 client.initialize()
